0a8231bbe52a9ac609180c6a607b976244cb0ca9
Janith Harshana <you@example.com>
2023-03-19 15:44:14 +0530

    Methmi 1

---
+++ app/src/main/java/com/example/login/Career_Guidence_Register.kt
@@ -0,0 +1,11 @@
+package com.example.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Career_Guidence_Register : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_career_guidence_register)
+    }
+}
