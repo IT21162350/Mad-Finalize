b62fd1ecb7dbdcb182c4cb29dec58bb5d0f72a70
Janith Harshana <you@example.com>
2023-03-19 16:10:27 +0530

    Methmi 2

---
+++ app/src/main/java/com/example/login/Notification_Page.kt
@@ -0,0 +1,11 @@
+package com.example.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Notification_Page : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_notification_page)
+    }
+}
