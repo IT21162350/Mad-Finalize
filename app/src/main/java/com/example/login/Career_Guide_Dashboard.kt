c3d3a42ddb38abbd924e0155b1339fd9cca4b5c7
Janith Harshana <you@example.com>
2023-03-19 17:24:44 +0530

    Methmi 3

---
+++ app/src/main/java/com/example/login/Career_Guide_Dashboard.kt
@@ -0,0 +1,11 @@
+package com.example.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Career_Guide_Dashboard : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_career_guide_dashboard)
+    }
+}
---
+++ app/src/main/java/com/example/login/job_application_2.kt
@@ -0,0 +1,11 @@
+package com.example.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class job_application_2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_job_application2)
+    }
+}
