04e92e89cad30dd9e2b0945fe3c0d9289e06d78d
Janith Harshana <you@example.com>
2023-03-19 11:26:38 +0530

    add job

---
+++ app/src/main/java/com/example/login/add_job.kt
@@ -0,0 +1,11 @@
+package com.example.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class add_job : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_job)
+    }
+}
